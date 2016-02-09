1a9245919a532baec10a32b4645abde9b991c7ce
Rogério Carvalho Schneider <stockrt@gmail.com>
2016-02-09 20:46:02 -0200

    Indent

--- dot/docker-activate.sh
+++ dot/docker-activate.sh
@@ -1,6 +1,5 @@
 [[ -f "$HOME/.docker-completion.bash" ]] && source "$HOME/.docker-completion.bash"
 [[ -f "$HOME/.docker-machine-completion.bash" ]] && source "$HOME/.docker-machine-completion.bash"
-
 export DOCKER_MACHINE_WRAPPED="true"
 [[ -f "$HOME/.docker-machine-wrapper.bash" ]] && source "$HOME/.docker-machine-wrapper.bash"
 eval $(docker-machine env)
