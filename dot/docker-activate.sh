9119b642a864d80f931b21640bbf374cdd199b77
Rogério Carvalho Schneider <stockrt@gmail.com>
2016-02-09 20:25:04 -0200

    Bash completion embedded for better compatibility with Mac and Linux

--- dot/docker-activate.sh
+++ dot/docker-activate.sh
@@ -1,5 +1,6 @@
-eval $(docker-machine env)
 [[ -f "$HOME/.docker-completion.bash" ]] && source "$HOME/.docker-completion.bash"
+[[ -f "$HOME/.docker-machine-completion.bash" ]] && source "$HOME/.docker-machine-completion.bash"
+
 export DOCKER_MACHINE_WRAPPED="true"
 [[ -f "$HOME/.docker-machine-wrapper.bash" ]] && source "$HOME/.docker-machine-wrapper.bash"
-[[ -f "$HOME/.docker-machine-completion.bash" ]] && source "$HOME/.docker-machine-completion.bash"
+eval $(docker-machine env)
