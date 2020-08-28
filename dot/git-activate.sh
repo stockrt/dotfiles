35fcb53f114507a400852d102fd13f3346d72cc4
Rogério Carvalho Schneider <stockrt@gmail.com>
2020-08-28 19:38:16 -0300

    PS1

--- dot/git-activate.sh
+++ dot/git-activate.sh
@@ -1,6 +1,6 @@
 source "$HOME/.git-completion.bash"
 source "$HOME/.git-prompt.sh"
-export PS1='\t \u@\h \w$(__git_ps1 " (%s)") \$ '
+export PS1='\t \[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(__git_ps1 "(%s)")\$ '
 export GIT_PS1_DESCRIBE_STYLE="branch"
 export GIT_PS1_SHOWCOLORHINTS="true"
 export GIT_PS1_SHOWDIRTYSTATE="true"
